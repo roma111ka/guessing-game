8fb5707e83f3f4fbdea86d84620cd12238dbeef1
Роман Лодяты Александрович <68326671+roma111ka@users.noreply.github.com>
2021-08-18 15:30:37 +0300

    feat: add class guessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.range;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.range = Math.round((this.min + this.max) / 2);
+        return this.range
     }
 
     lower() {
-
+        this.max = this.range;
     }
 
     greater() {
-
+        this.min = this.range;
     }
 }
 
